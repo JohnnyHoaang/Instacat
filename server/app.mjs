afa970b44d0ae3dc1256a08539d77a328b7400b2
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-01-29 21:08:36 -0500

    Create empty hello page

--- server/app.mjs
+++ server/app.mjs
@@ -2,7 +2,9 @@ import express from 'express'
 import router from './routes/all.mjs'
 
 const app = express()
-app.use("/", router)
+
 app.use(express.static('../client/build'))
+app.use("/", router)
+
 
 export default app
