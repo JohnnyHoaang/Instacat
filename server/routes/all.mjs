5af6656c8ab2bf4fe9788e6be63ace3b985e7eae
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-02-02 17:56:32 -0500

    Add post api routes for user comments and connect to db

--- server/routes/all.mjs
+++ server/routes/all.mjs
@@ -1,4 +1,6 @@
 import express from 'express'
+import Database from '../db/db.mjs'
+import dotenv from 'dotenv'
 
 const router = express.Router()
 router.use(express.json())
@@ -12,6 +14,35 @@ router.get('/hello', async(req,res)=>{
     }
 })
 
+router.post('/', async (req, res)=>{
+    console.log(req.body)
+    console.log("username: " + req.body.username)
+    console.log("comment: " + req.body.comment)
+    await insertDB("test","usercomments", req.body)
+})
+
+router.post('/upload', (req, res) => {
+    console.log(req.body)
+})
+
+router.get('/usercomments', async (req, res)=>{
+    let db = new Database()
+    await db.connect("test", "usercomments")
+    let data = await db.readAll()
+    db.close()
+    res.json(data)
+})
 router.use((req,res) => res.status(404).json({ error : "error"}))
 
+async function insertDB(dbName, collName, body){
+    let db = new Database()
+    await db.connect(dbName, collName, body)
+    await db.addData(body)
+    let data = await db.readAll()
+    db.close()
+    console.log("Returned data:")
+    console.log(data)
+}
+
 export default router
+
