5cec5b3ac234aab0738c2ab8e576528c1543b0ee
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2023-02-05 19:51:41 -0500

    added mongo object

--- server/db/db.mjs
+++ server/db/db.mjs
@@ -2,7 +2,29 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config()
 
-async function main() {
+async function connect() {
     await mongoose.connect(process.env.ATLAS_URI)
 }
-main()
+
+async function disconnect() {
+    await mongoose.disconnect()
+}
+
+class Mongo {
+    constructor() {
+        if (Mongo._instance) {
+          return Mongo._instance
+        }
+        Mongo._instance = this;    
+    }
+    
+    async connect() {
+        await mongoose.connect(process.env.ATLAS_URI)
+    }
+    
+    async disconnect() {
+        await mongoose.disconnect()
+    }
+}
+
+export { Mongo }
