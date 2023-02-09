fdb8d85e94cd6c48bebb41d9f4420963eeeae18e
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2023-02-09 12:51:32 -0500

    added file for api route

--- server/app.mjs
+++ server/app.mjs
@@ -2,13 +2,16 @@ import express from 'express'
 import main from './routes/main.mjs'
 import comment from './routes/comment.mjs'
 import profile from './routes/profile.mjs'
+import api from "./routes/api.mjs"
 
 const app = express()
 app.use(express.static('../client/build')) 
 
 app.use("/comment", comment)
 app.use("/profile", profile)
+app.use("/api", api)
 app.use("/", main)
+
 
 
 
