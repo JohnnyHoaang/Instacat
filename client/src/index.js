c6b196af4349382b945e6cb6a4ad8712e670bb60
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-02-09 12:48:20 -0500

    Add Mongoose post model

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const sasToken = process.env.AZURE_SAS;
-const containerName = 'helloblob';
-const storageAccountName = process.env.storagereousrcename;
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
