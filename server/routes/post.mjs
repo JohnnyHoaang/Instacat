e81e2cedab9b27a703991de13ce10a6b2a8c89da
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-02-13 11:28:25 -0500

    Add API for all posts

--- server/routes/post.mjs
+++ server/routes/post.mjs
@@ -9,6 +9,8 @@ router.use(express.json())
 router.get('/cat/id/:id', async (req, res) => {
     await sendAPI(res, Post, { id : req.params.id })
 })
-
+router.get('/cat/all/', async (req, res)=>{
+    await sendAPI(res, Post, {})
+})
 export default router
 
