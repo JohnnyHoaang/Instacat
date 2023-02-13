52a5d0d27149e66b86febebf8b7ea8de74f0d72c
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-02-13 17:30:44 -0500

    Express routes do not block react route anymore

--- server/routes/main.mjs
+++ server/routes/main.mjs
@@ -1,5 +1,7 @@
 import express from 'express'
+import path from 'path'
 
+const __dirname = path.resolve();
 const router = express.Router()
 
 router.use(express.json())
@@ -12,6 +14,10 @@ router.get('/hello', async (req, res) => {
         res.status(500).json({ error: "server error" })
     }
 })
+
+router.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname,'../client/build/index.html'));
+});
 
 router.use((req, res) => res.status(404).json({ error: "error" }))
 
