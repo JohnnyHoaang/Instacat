76bb2a81542480f44baafd42fd450465c80193f8
maedeh-hsn <maedeh.hassani@dawsoncollege.qc.ca>
2023-02-13 01:51:24 -0500

    adding (api) as backecnd, conecting to backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,27 @@ import Header from './Header/Header.js';
 import Main from './Main/MainContainer.js'
 import Footer from './Footer/Footer.js';
 import Navigation from './Navigation/Navigation.js';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let [apiResponse, setApiResponse] = useState(null);
+
+  useEffect(() => {
+            
+    let url = "http://localhost:9000/testAPI";
+    fetch(url)
+    .then(response => response.text())
+    .then(data => setApiResponse(data))
+    .catch(err => console.log(err.message))
+    }, []);
   
     return(
       <div className="App">
         <Header />
         <Navigation />
         <Main />
+        <p className="App-itro">this is from server: {apiResponse}</p>
         <Footer />
       </div>
     );
