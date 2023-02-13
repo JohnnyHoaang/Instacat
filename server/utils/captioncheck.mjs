36f5624e12ea42e5fd6bb166902f5d55e9b20047
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-02-13 18:49:31 -0500

    Add a hashtag check for caption

--- server/utils/captioncheck.mjs
+++ server/utils/captioncheck.mjs
@@ -1,5 +1,7 @@
+// return words starting with a hashtag with a given caption
 function lookForHashtags(caption){
-    return []
+    let words = caption.split(' ')
+    return words.filter(x => x[0] == '#').map(x => x.slice(1))
 }
 
 export { lookForHashtags }
