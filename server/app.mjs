d4f1b957331bbdbce9aaf0150b71372725d2f110
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-02-14 14:40:22 -0500

    Remove unused imports

--- server/app.mjs
+++ server/app.mjs
@@ -1,14 +1,10 @@
 import express from 'express'
 import main from './routes/main.mjs'
-import comment from './routes/comment.mjs'
-import profile from './routes/profile.mjs'
 import posts from './routes/post.mjs'
 import addPost from './routes/uploadpost.mjs'
 const app = express()
 app.use(express.static('../client/build')) 
 
-app.use("/comment", comment)
-app.use("/profile", profile)
 app.use("/api", posts)
 app.use("/add", addPost)
 app.use("/", main)
