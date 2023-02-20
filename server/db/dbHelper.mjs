cc097746d27b3ea0f415f5b7d5d580e5c01ad013
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-02-20 10:49:08 -0500

    Add jest test for all post route

--- server/db/dbHelper.mjs
+++ server/db/dbHelper.mjs
@@ -1,33 +1,41 @@
-/**
+let _instance = null
+class DBHelper {
+    constructor() {
+        if (_instance) {
+            return _instance
+        }
+        _instance = this;
+    }
+    /**
  * Inserts model data to DB.
  * @param {Object} response 
  * @param {Model} model 
  * @param {Object} body 
  * @author Johnny Hoang
  */
-async function insertToDB(response, model, body) {
-    const user = model(body)
-    try {
-        await user.save()
-    } catch (error) {
-        response.status(500).send(error)
+    async insertToDB(response, model, body) {
+        const user = model(body)
+        try {
+            await user.save()
+        } catch (error) {
+            response.status(500).send(error)
+        }
     }
-}
-/**
- * Sends back API result with given model and query.
- * @param {Object} response 
- * @param {Model} model 
- * @param {Object} query 
- * @author Johnny Hoang
- */
-async function sendAPI(response, model, query) {
-    const data = await model.find(query)
-    try {
-        response.send(data);
-    } catch (error) {
-        console.log(error)
-        response.status(500).send(error);
+    /**
+     * Returns query result with given model and query.
+     * @param {Model} model 
+     * @param {Object} query 
+     * @author Johnny Hoang
+     */
+    async getQueryData(model, query) {
+        const data = await model.find(query)
+        try {
+            return data
+        } catch (error) {
+            console.log(error)
+            response.status(500).send(error);
+        }
     }
-}
 
-export { insertToDB, sendAPI }
+}
+export { DBHelper }
