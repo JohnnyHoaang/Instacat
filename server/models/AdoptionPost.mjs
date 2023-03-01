01a63238c6da968efa3aa0f4083546f758f65a01
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-03-01 12:02:37 -0500

    Add model for adoption posts

---
+++ server/models/AdoptionPost.mjs
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose'
+
+const AdoptionPostSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+    },
+    gender: {
+        type: String,
+        default: "",
+    },
+    size: {
+        type: String,
+        default: "",
+    },
+    coat: {
+        type: String,
+        default: "",
+    },
+    tags: {
+        type: Array,
+        default: [],
+    },
+    name: {
+        type: String,
+        default: "",
+    },
+    primary_photo_cropped: {
+        small: {
+            type: String
+        },
+        medium: {
+            type: String,
+            default: ""
+        },
+        large: {
+            type: String,
+            default: ""
+        },
+        full: {
+            type: String,
+            default: ""
+        }
+    },
+    status: {
+        type: String,
+        default: "adoptable"
+    }
+});
+
+const AdoptionPost = mongoose.model("AdoptionPost", AdoptionPostSchema);
+
+export { AdoptionPost }
+
