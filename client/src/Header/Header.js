e20e171e1a0e90cddc0cff08338d18b0a58fdc2d
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2023-03-06 10:27:32 -0500

    added logout route

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 import myLogo from '../images/logo01.png'
 import defaultProfile from '../images/default-profile.png'
@@ -8,8 +8,10 @@ import { GoogleLogin } from '@react-oauth/google';
  
 const Header = () => {
 
+    const [username, setUsername] = useState("")
+
     const handleLogin = async googleData => {
-        const res = await fetch("/auth", {
+        const res = await fetch("/auth/login", {
             method: "POST",
             body: JSON.stringify({
             token: googleData.credential
@@ -19,23 +21,33 @@ const Header = () => {
           }
         })
         const data = await res.json()
+        console.log(data)
         // we will come back to this, since our server will be replying with our info
-      }
+        setUsername(data.user)
+    }
       
     const handleError = error => {
         console.error(error);
     }
+
+    const handleLogout = async () => {
+        await fetch("/auth/logout");
+        setUsername("");
+      }
+
 
     return (
         <header>
             <img src={myLogo} alt="logo" id="logo"></img>
             {/* <h1>InstaCat</h1> */}
                 <div id='profile-div'>
-                    <GoogleLogin 
+                    <img src={defaultProfile} alt="profile" id="profile-img"></img>
+                    {!username && <GoogleLogin 
                         onSuccess={handleLogin}
                         onError={handleError}
-                    />
-                    <img src={defaultProfile} alt="profile" id="profile-img"></img>
+                    />}
+                    {username && <button onClick={handleLogout}>Logout</button>}
+                    <p>{username ? username : "Guest"}</p>
                 </div>            
         </header>
     );
--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -5,20 +5,35 @@
 import express from "express"
 import { DBHelper } from '../db/dbHelper.mjs'
 import { User } from '../models/User.mjs'
+import { OAuth2Client } from "google-auth-library"
+import dotenv from 'dotenv'
+import session from "express-session"
+
 const db = new DBHelper()
-
-const {OAuth2Client} = require('google-auth-library');
-
-const dotenv = require('dotenv')
 dotenv.config()
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 const router = express.Router()
-const session = require('express-session');
+
+// TODO replace later when users are inserted to database
+const users = new Array()
 
 router.use(express.json())
 
-router.post("/", async (req, res) => {
+router.use(session({
+    secret: process.env.SECRET,
+    name: 'id',
+    saveUninitialized: false,
+    resave: false,
+    cookie: { 
+      maxAge: 120000,
+      secure: false, 
+      httpOnly: true,
+      sameSite: 'strict'
+    }
+  }))  
+
+router.post("/login", async (req, res) => {
     const {token} = req.body
     const ticket = await client.verifyIdToken({
         idToken: token,
@@ -28,30 +43,51 @@ router.post("/", async (req, res) => {
         return res.sendStatus(401)
     const { name, email, picture } = ticket.getPayload()
 
+    //TODO insert user to the database
+    const user = {"name": name, "email": email, "picture": picture}
+    const existsAlready = users.findIndex(element => element.email === email)
+    if (existsAlready < 0) {
+      users.push(user)
+    } else {
+      users[existsAlready] = user
+    }
+
     //TODO add picture data
-    await db.insertToDB(res, User, {name: name, email: email})
+
+    // await db.insertToDB(res, User, {name: name, email: email})
 
     req.session.regenerate((err) => {
         if (err) {
             return res.sendStatus(500)
         }
-        req.session.user = email // user is a var used when the database portion is implemented
-        res.json({user: email})
+        req.session.user = user // user is a var used when the database portion is implemented
+        res.json({user: user})
     })
 
 })
 
-router.use(session({
-  secret: process.env.SECRET,
-  name: 'id',
-  saveUninitialized: false,
-  resave: false,
-  cookie: { 
-    maxAge: 120000,
-    secure: secure, 
-    httpOnly: true,
-    sameSite: 'strict'
+router.get("/logout", isAuthenticated, () => {
+  req.session.destroy(function(err) {
+    if (err) {
+      return res.sendStatus(500)
+    }
+    res.clearCookie('id')
+    res.sendStatus(200)
+  })
+})
+
+/**
+ * Function to check if a user can access a route
+ * @param {*} req Request object
+ * @param {*} res Result
+ * @param {*} next Next function
+ * @returns 
+ */
+function isAuthenticated(req, res, next) {
+  if (!req.session.user){
+    return res.sendStatus(401); //unauthorized
   }
-}))
+  next();
+}
 
 export default router
