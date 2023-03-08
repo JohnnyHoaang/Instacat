d95f5a59b096deb99b469ee870db7b6ef5990e2d
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2023-03-08 12:32:57 -0500

    added envars so azure can see them

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,7 @@ az webapp config appsettings set --resource-group $RESOURCE_GROUP_NAME --name $A
 az webapp config appsettings set --resource-group $RESOURCE_GROUP_NAME --name $APP_NAME --settings ATLAS_URI=$ATLAS_URI
 az webapp config appsettings set --resource-group $RESOURCE_GROUP_NAME --name $APP_NAME --settings CONTAINER_NAME=$CONTAINER_NAME
 az webapp config appsettings set --resource-group $RESOURCE_GROUP_NAME --name $APP_NAME --settings AZURE_SAS=$AZURE_SAS
+az webapp config appsettings set --resource-group $RESOURCE_GROUP_NAME --name $APP_NAME --settings GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID
+az webapp config appsettings set --resource-group $RESOURCE_GROUP_NAME --name $APP_NAME --settings SECRET=$SECRET
+az webapp config appsettings set --resource-group $RESOURCE_GROUP_NAME --name $APP_NAME --settings CLIENT_SECRET=$CLIENT_SECRET
 az webapp deployment source config-zip --resource-group $RESOURCE_GROUP_NAME --name $APP_NAME --src deploy.zip
