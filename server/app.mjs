394420e18b381f378c975bc96f3aae10f9488670
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-03-23 13:19:50 -0400

    Merge with dev

---
+++ server/app.mjs
@@ -0,0 +1,19 @@
+import express from 'express'
+import main from './routes/main.mjs'
+import posts from './routes/post.mjs'
+import likes from './routes/likes.mjs'
+import addPost from './routes/uploadpost.mjs'
+import auth from './routes/auth.mjs'
+import editProfile from './routes/uploadprofile.mjs'
+import admin from './routes/adminCheck.mjs'
+const app = express()
+app.use(express.static('../client/build'))
+
+app.use("/api", posts)
+app.use("/add", addPost)
+app.use("/auth", auth)
+app.use("/update", likes)
+app.use("/edit/profile", editProfile)
+app.use("/", main)
+
+export default app
