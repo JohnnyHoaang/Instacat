1fa1705cf09c26bede4fd4e07165361940c57015
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-03-23 15:06:59 -0400

    fixing merge

--- server/app.mjs
+++ server/app.mjs
@@ -1,9 +1,22 @@
+<<<<<<< HEAD
 import express from 'express';
 import main from './routes/main.mjs';
 import posts from './routes/post.mjs';
 import likes from './routes/likes.mjs';
 import addPost from './routes/uploadpost.mjs';
 import auth from './routes/auth.mjs';
+=======
+import express from 'express'
+import main from './routes/main.mjs'
+import posts from './routes/post.mjs'
+import likes from './routes/likes.mjs'
+import addPost from './routes/uploadpost.mjs'
+import auth from './routes/auth.mjs'
+import editProfile from './routes/uploadprofile.mjs'
+import admin from './routes/adminCheck.mjs'
+const app = express()
+app.use(express.static('../client/build'))
+>>>>>>> parent of e814a3e (Remove inexistent import)
 
 const app = express();
 app.use(express.static('../client/build'));
