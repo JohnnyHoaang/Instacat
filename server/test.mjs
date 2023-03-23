cf2ff0ed05ab0c2cedb554b7632975e07d006d4d
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-03-23 18:04:19 -0400

    Remove queries

--- server/test.mjs
+++ server/test.mjs
@@ -12,15 +12,6 @@ async function main() {
 
   process.on('SIGTERM', shutdown)
   process.on('SIGINT', shutdown)
-  // await User.deleteMany({email:"jhoangbt@gmail.com"})
-  // await User.deleteMany({email:"johnnyhoangbt@gmail.com"})
-  // await User.deleteMany({email:"2003kelsey@gmail.com"})
-  // await User.deleteMany({email:"dan.pomerantz@gmail.com"})
-  // await User.deleteMany({email:"bogdivan2002@gmail.com"})
-  db.updateData(User, {email:"jhoangbt@gmail.com"}, { isAdmin: true})
-  // db.updateData(User, {email:"dan.pomerantz@gmail.com"}, { isAdmin: true})
-  // db.updateData(User, {email:"bogdivan2002@gmail.com"}, { isAdmin: true})
-  // db.updateData(User, {email:"2003kelsey@gmail.com"}, { isAdmin: true})
   console.log(await User.find({}))
   async function shutdown() {
     console.log("Shutting down");
