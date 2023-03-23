03e75d213da852c04c27ddb98bee6892362d4ed3
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2023-03-23 13:40:11 -0400

    Add post route for admin check

--- server/routes/adminCheck.mjs
+++ server/routes/adminCheck.mjs
@@ -7,14 +7,10 @@ router.use(express.json())
 // route to check for admin
 router.post('/check', async (req, res) => {
     const email = req.body.email
+    // Find user with email
     let user = await User.find({email: email})
-    let response = {}
-    // if(user.isAdmin == true){
-    //     response = { isAdmin: true }
-    // } else {
-    //     response = { isAdmin: false }
-    // }
-    res.status(201).send({isAdmin: user[0]})
+    // Returns if user is admin or not
+    res.status(201).send({isAdmin: user[0].isAdmin})
 })
 
 export default router
