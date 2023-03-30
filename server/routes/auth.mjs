08eb75acab44f9f0784c5a50323ff2623a11d20e
Kelsey <kelsey.pereiracosta@dawsoncollege.qc.ca>
2023-03-30 13:03:26 -0400

    moved isAuthenticated to utils

--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -8,6 +8,7 @@ import {User} from '../models/User.mjs';
 import {OAuth2Client} from 'google-auth-library';
 import dotenv from 'dotenv';
 import session from 'express-session';
+import isAuthenticated from '../utils/util.mjs';
 
 const db = new DBHelper();
 dotenv.config();
@@ -30,63 +31,56 @@ router.use(session({
   },
 }));
 
-router.post("/login", async (req, res) => {
-    const { token } = req.body
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.GOOGLE_CLIENT_ID
-    })
-    if (!ticket)
-        return res.sendStatus(401)
-    const { name, email, picture } = ticket.getPayload()
-    // Check if user exists with email
-    let user = await User.find({email: email})
-    if(user.length == 0){
-        // Create new user object and insert to database
-        const user = { "name": name, "email": email, "picture": picture }
-        db.insertToDB(User, user)
-        // Regenerate session with new user
-        regenerateSession(req, res, user)
-    } else {
-        // Regenerate session with user from DB
-        regenerateSession(req,res,user[0])
+router.post('/login', async (req, res) => {
+  const {token} = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+  if (!ticket) {
+    return res.sendStatus(401);
+  };
+  const {name, email, picture} = ticket.getPayload();
+  // Check if user exists with email
+  const user = await User.find({email: email});
+  if (user.length == 0) {
+    // Create new user object and insert to database
+    const user = {'name': name, 'email': email, 'picture': picture};
+    db.insertToDB(User, user);
+    // Regenerate session with new user
+    regenerateSession(req, res, user);
+  } else {
+    // Regenerate session with user from DB
+    regenerateSession(req, res, user[0]);
+  }
+  // TODO add picture data
+});
+
+router.get('/logout', isAuthenticated, (req, res) => {
+  req.session.destroy(function(err) {
+    if (err) {
+      return res.sendStatus(500);
     }
-    //TODO add picture data
-
-})
-
-router.get("/logout", isAuthenticated, (req, res) => {
-    req.session.destroy(function (err) {
-        if (err) {
-            return res.sendStatus(500)
-        }
-        res.clearCookie('id')
-        res.sendStatus(200)
-    })
-})
-
-function regenerateSession(req, res, user) {
-    req.session.regenerate((err) => {
-        if (err) {
-            return res.sendStatus(500)
-        }
-        req.session.user = user
-        res.json({ user: user })
-    })
-}
+    res.clearCookie('id');
+    res.sendStatus(200);
+  });
+});
 
 /**
- * Function to check if a user can access a route
- * @param {*} req Request object
- * @param {*} res Result
- * @param {*} next Next function
- * @return {status} Status 401 if the user isn't authenticated
+ * Regenerates a session
+ * @param {*} req
+ * @param {*} res
+ * @param {*} user
  */
-function isAuthenticated(req, res, next) {
-  if (!req.session.user) {
-    return res.sendStatus(401); // unauthorized
-  }
-  next();
+function regenerateSession(req, res, user) {
+  req.session.regenerate((err) => {
+    if (err) {
+      return res.sendStatus(500);
+    }
+    req.session.user = user;
+    res.json({user: user});
+  });
 }
+
 
 export default router;
