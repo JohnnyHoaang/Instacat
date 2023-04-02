2212c5e11a4ba7a6864fad77142b06dc36268286
Maedeh Hassani <maedeh.hassani@dawsoncollege.qc.ca>
2023-04-02 00:44:18 -0400

    fix pagination issu for hastag with 5

--- client/src/Hashtag/SameHashtag.js
+++ client/src/Hashtag/SameHashtag.js
@@ -17,7 +17,7 @@ function SameHashtag () {
     const { hashtag } = useParams();
     let [eachHashtag, setEachHashtag]= useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const cardsPerPage = 4;
+    const cardsPerPage = 5;
 
     useEffect(() => {
         let url = `/api/hashtag/${hashtag}`;
